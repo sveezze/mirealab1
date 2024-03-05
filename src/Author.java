90258d05f32fe5425a8f9255ccfc9a810986fa6c
sveezze1 <sveezze1@gmail.com>
2024-03-05 20:29:08 +0300

    2.1

---
+++ src/Author.java
@@ -0,0 +1,35 @@
+public class Author {
+    private String name;
+    private String email;
+    private char gender;
+
+    // Конструктор
+    public Author(String name, String email, char gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    // Методы аксессоры
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    // Метод toString
+    @Override
+    public String toString() {
+        return "Author[name=" + name + ",email=" + email + ",gender=" + gender + "]";
+    }
+}
