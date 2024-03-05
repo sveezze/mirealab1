040f9b1e7728eb2ae6ba148ea6f7422f3282a6b4
sveezze1 <sveezze1@gmail.com>
2024-03-05 20:12:11 +0300

    1.6

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,10 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("Первые 10 чисел гармонического ряда:");
 
-        System.out.print("Введите количество аргументов командной строки: ");
-        int numArgs = scanner.nextInt();
-
-        String[] commandLineArgs = new String[numArgs];
-
-        System.out.println("Введите аргументы командной строки:");
-
-        for (int i = 0; i < numArgs; i++) {
-            System.out.print("Аргумент " + (i + 1) + ": ");
-            commandLineArgs[i] = scanner.next();
-        }
-
-        System.out.println("Введенные аргументы командной строки:");
-
-        for (String arg : commandLineArgs) {
-            System.out.println(arg);
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%.4f%n", 1.0 / i);
         }
     }
 }
