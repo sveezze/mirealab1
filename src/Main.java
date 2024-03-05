c6ae9ee594ed9d10a1f443b14b4b51b949c68665
sveezze1 <sveezze1@gmail.com>
2024-03-05 19:59:59 +0300

    1.4

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,32 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5}; // можно заменить на ввод с клавиатуры
-        int sum = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите размер массива: ");
+        int size = scanner.nextInt();
 
-        for (int i = 0; i < array.length; i++) {
+        int[] array = new int[size];
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        System.out.println("Введите элементы массива:");
+
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
             sum += array[i];
+
+            if (array[i] > max) {
+                max = array[i];
+            }
+
+            if (array[i] < min) {
+                min = array[i];
+            }
         }
 
-        double average = (double) sum / array.length;
-
         System.out.println("Сумма элементов массива: " + sum);
-        System.out.println("Среднее арифметическое элементов массива: " + average);
+        System.out.println("Максимальный элемент массива: " + max);
+        System.out.println("Минимальный элемент массива: " + min);
     }
 }
